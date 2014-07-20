4d2fc1018e359f10e92efb9985f2bbf564c03389
Patrick Bogen <pbogen@twitter.com>
2014-07-19 18:51:21 -0700

    Add a season 2 scenario

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -2,6 +2,7 @@ Scenario.create( [
 	{ name: 'Silent Tide', low_tier_lower: 1, low_tier_upper: 2, high_tier_lower: 4, high_tier_upper: 5, season: 0, episode: 1 },
 	{ name: "The Hydra's Fang Incident", low_tier_lower: 1, low_tier_upper: 2, high_tier_lower: 4, high_tier_upper: 5, season: 0, episode: 2 },
 	{ name: "Voice in the Void", low_tier_lower: 1, low_tier_upper: 2, mid_tier_lower: 3, mid_tier_upper: 4, high_tier_lower: 6, high_tier_upper: 7, season: 1, episode: 35 },
+	{ name: "Before the Dawn Pt.1", low_tier_lower: 1, low_tier_upper: 2, mid_tier_lower: 3, mid_tier_upper: 4, high_tier_lower: 6, high_tier_upper: 7, season: 2, episode: 1 }
 ] )
 
 User.create!( [ { 
