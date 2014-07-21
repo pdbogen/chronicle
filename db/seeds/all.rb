be1d3947e899c057b19c2cd559e59b6da61ab366
Patrick Bogen <pbogen@twitter.com>
2014-07-21 12:07:29 -0700

    Seed a few scenarios to all environments

--- db/seeds/all.rb
+++ db/seeds/all.rb
@@ -1,1 +1,8 @@
+Scenario.create( [
+	{ name: 'Silent Tide', low_tier_lower: 1, low_tier_upper: 2, high_tier_lower: 4, high_tier_upper: 5, season: 0, episode: 1 },
+	{ name: "The Hydra's Fang Incident", low_tier_lower: 1, low_tier_upper: 2, high_tier_lower: 4, high_tier_upper: 5, season: 0, episode: 2 },
+	{ name: "Voice in the Void", low_tier_lower: 1, low_tier_upper: 2, mid_tier_lower: 3, mid_tier_upper: 4, high_tier_lower: 6, high_tier_upper: 7, season: 1, episode: 35 },
+	{ name: "Before the Dawn Pt.1", low_tier_lower: 1, low_tier_upper: 2, mid_tier_lower: 3, mid_tier_upper: 4, high_tier_lower: 6, high_tier_upper: 7, season: 2, episode: 1 }
+] )
+
 Right.create( [ { right_name: 'Manage Users' }, { right_name: 'Manage Scenarios' } ] )
